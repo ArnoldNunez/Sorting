f409e1ff67047f9a69b8f0ef96e8e64f34773879
Arnold Nunez <Arnold Nunez>
2016-07-21 09:43:31 -0400

    Setting up Project Structure

---
+++ Sorting/SortingLib.h
@@ -0,0 +1,18 @@
+/**
+ * \file SortingLib.h
+ *
+ * \author Arnold Nunez
+ *
+ * \brief Functions that will demonstrate the usages
+ *		of different sorting algorithms
+ */
+
+/**
+ * Lets contain our functions that we will use
+ * int a namespace called Sorting
+ */
+namespace Sorting{
+
+	void InsertionSort();
+
+}
---
+++ Sorting/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * \file main.cpp
+ *
+ * \author Arnold Nunez
+ */
+
+
+int main(int argc, char* argv[])
+{
+
+}
